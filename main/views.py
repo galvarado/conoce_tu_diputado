f711e7a34860d460ea2777bf4f621ce192ea085f
Guillermo <stallion.ak47@gmail.com>
2012-08-23 19:46:10 -0500

    search table

--- main/views.py
+++ main/views.py
@@ -8,3 +8,32 @@ from django.db.models import Q
 
 def index(request):
     return render_to_response('index.html', RequestContext(request))
+
+def get_deputies(request):
+    '''
+    Retrive deputies table
+    '''
+    aaData = []
+    start = request.POST.get('iDisplayStart')
+    display_length = request.POST.get('iDisplayLength')
+    end = start + display_length
+    search = request.POST.get('sSearch')
+    users = []
+    count = users.count()
+
+    for user in users[start:end]:
+        group = 'Sin asignar' if user.profile.group is None else user.profile.group.name
+        aaData.append([
+            user.username,
+            user.email,
+            group,
+            '<input type="checkbox" data-id="%s">' % user.pk,
+        ])
+    data = {
+        "iTotalRecords": count,
+        "iDisplayStart": start,
+        "iDisplayLength": 0,
+        "iTotalDisplayRecords": count,
+        "aaData":aaData
+    }
+    return HttpResponse(json.dumps(data))
