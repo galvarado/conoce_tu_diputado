a70ae1f951a8b6137b18c279ab0ca8158b75294c
Guillermo <stallion.ak47@gmail.com>
2012-08-24 12:20:32 -0500

    Specific search page

--- main/views.py
+++ main/views.py
@@ -46,3 +46,38 @@ def get_deputies(request):
         "aaData":aaData
     }
     return HttpResponse(json.dumps(data))
+
+def search(request):
+    '''
+    Show search page
+    '''
+    return render_to_response('search.html', RequestContext(request))
+
+def get_deputies_search(request):
+    '''
+    Retrive deputies specific search table
+    '''
+    aaData = []
+    start = 0
+    display_length = 0
+    end = 0
+    search = request.POST.get('sSearch')
+    users = []
+    count = 0
+
+    for user in users[start:end]:
+        group = 'Sin asignar' if user.profile.group is None else user.profile.group.name
+        aaData.append([
+            user.username,
+            user.email,
+            group,
+            '<input type="checkbox" data-id="%s">' % user.pk,
+        ])
+    data = {
+        "iTotalRecords": count,
+        "iDisplayStart": start,
+        "iDisplayLength": display_length,
+        "iTotalDisplayRecords": count,
+        "aaData":aaData
+    }
+    return HttpResponse(json.dumps(data))
